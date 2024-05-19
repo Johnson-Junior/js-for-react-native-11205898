4cf65f562c9b502cb598daddf99f69eb0a066747
JohnsonJunior <johnsonkrejnr@gmail.com>
2024-05-19 01:23:20 +0000

    Second Commit

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,4 +1,13 @@
 function processArray(arr) {
     return arr.map(num => num % 2 === 0 ? num ** 2 : num * 3);
   }
+  function processArray(arr) {
+    return arr.map(num => num % 2 === 0 ? num ** 2 : num * 3);
+  }
   
+  function formatArrayStrings(stringArr, numArr) {
+    return stringArr.map((str, index) => {
+      const num = numArr[index];
+      return num % 2 === 0 ? str.toUpperCase() : str.toLowerCase();
+    });
+  }
